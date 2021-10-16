2bfb24cd524a4f655ba15ecbd0d846bde3b31454
MartinReynolds <muradjon@yahoo.co.uk>
2021-10-16 15:28:04 +0100

    experiment on dynamic image

--- src/pages/trial/DynamicImage.tsx
+++ src/pages/trial/DynamicImage.tsx
@@ -2,6 +2,8 @@ import { CreateAnimation, Animation } from '@ionic/react';
 import { IonCol, IonContent, IonGrid, IonHeader, IonPage, IonRow, IonTitle, IonToolbar } from '@ionic/react';
 import Square from '../../components/trial/Square';
 import './DynamicImage.css';
+// import { Script } from '..\\..\\scripts\\DynamicImage.js';
+
 
 const DynamicImage: React.FC = () => {
   return (
